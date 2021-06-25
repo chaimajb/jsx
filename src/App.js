b296a18089de5e989c345bac1621098c58a5f78a
chaimajb <jabrichaima1@gmail.com>
2021-06-25 01:02:12 +0100

    commit jsx

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './Style.css';
+import './App.css';
+import mysourceimage from "./ImageInSrc.jpg";
+function App() {
+  return (
+    <div className="App">
+       <div style={{border:'solid 1px black',maxWidth:'100vw'}}>
+          <h1 className='title red'>Chaima</h1>
+          <img src={mysourceimage} alt ='Totilas' width='250' /> 
+          <br/>
+          <img src="/imageInPublic.jpg" width='250' ></img>
+          <br/>
+          <video 
+              width="320" 
+              height="240" 
+              controls 
+              src="myVideo.mp4" 
+              type="video/mp4">
+          </video>
+       </div>
+    </div>   
+  );
+}
+
+export default App;
